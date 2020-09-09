28800ce2832e4514904d7fa0d474df78bb008743
August <august.agren@elev.ga.ntig.se>
2020-09-09 10:51:46 +0200

    uppstart av modellerings övning#1

---
+++ Modellering#1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Modellering_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
